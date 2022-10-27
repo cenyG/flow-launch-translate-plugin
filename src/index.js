228079ba0159b590f67f197e65b1fcb69ba1e8d2
cenyg <test@test.test>
2022-10-27 08:11:45 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -22,66 +22,79 @@ Promise.resolve().then(async () => {
 })
 
 async function main(method, params, from, to) {
-    if (method === 'query' && (params.length === 0 || (params.length === 1 && !params[0]))) {
-        return response(DEFAULT_RESULT)
-    } else if (method === 'query' && params.length > 0 && params[0].startsWith(':set')) {
-        const split = params[0].trim().split(' ')
-
-        if (split.length === 3) {
-            const from = split[1]
-            const to = split[2]
-            return response([
-                {
-                    Title: 'Translate',
-                    Subtitle: `Set translate from ${from.toUpperCase()} to ${to.toUpperCase()}`,
-                    JsonRPCAction: {
-                        method: 'setLanguage',
-                        parameters: [from, to],
-                    },
-                    IcoPath: ICON_PATH,
-                },
-            ])
-        } else {
-            return response([
-                {
-                    Title: 'Translate',
-                    Subtitle: '`From` and `To` languages should be words of length 2 `en`,`de` etc.',
-                    IcoPath: ICON_PATH,
-                },
-            ])
-        }
-    } else if (method === 'setLanguage') {
-        return fs.writeFileSync(CONFIG_FILE_PATH, params.join(','))
-    } else if (method === 'copy') {
-        return clipboard.writeSync(params.join())
-    } else if (method === 'query' && params.length !== 0) {
-        let text = params.join(' ').trim()
-        const isReverse = text.startsWith('r ')
-
-        if (isReverse && text.length > 2) {
-            text = text.substring(2)
-            const tmp = from
-            from = to
-            to = tmp
-        }
-
-        if (text) {
-            const res = await translate(text.trim(), { to })
-
-            return response([
-                {
-                    Title: res.text,
-                    Subtitle: `${from} -> ${to} parameters`,
-                    IcoPath: ICON_PATH,
-                    JsonRPCAction: {
-                        method: 'copy',
-                        parameters: [res.text],
-                        dontHideAfterAction: false,
-                    },
-                },
-            ])
-        } else {
+    switch (method) {
+        case 'query':
+            if (params.length > 0 && params[0].startsWith(':set')) {
+                return setLanguage(params)
+            } else if (params.length > 0 && params[0]) {
+                return await performTranslate(params, from, to)
+            } else {
+                return response(DEFAULT_RESULT)
+            }
+        case 'setLanguage':
+            return fs.writeFileSync(CONFIG_FILE_PATH, params.join(','))
+        case 'copy':
+            return clipboard.writeSync(params.join())
+        default:
             return response(DEFAULT_RESULT)
-        }
+    }
+}
+
+function setLanguage(params) {
+    const split = params[0].trim().split(' ')
+
+    if (split.length === 3) {
+        const from = split[1]
+        const to = split[2]
+        return response([
+            {
+                Title: 'Translate',
+                Subtitle: `Set translate from ${from.toUpperCase()} to ${to.toUpperCase()}`,
+                JsonRPCAction: {
+                    method: 'setLanguage',
+                    parameters: [from, to],
+                },
+                IcoPath: ICON_PATH,
+            },
+        ])
+    } else {
+        return response([
+            {
+                Title: 'Translate',
+                Subtitle: 'Set translate `From` and `To` languages `en`,`de` etc.',
+                IcoPath: ICON_PATH,
+            },
+        ])
+    }
+}
+
+async function performTranslate(params, from, to) {
+    let text = params.join(' ').trim()
+    const isReverse = text.startsWith('r ')
+
+    if (isReverse && text.length > 2) {
+        text = text.substring(2)
+        const tmp = from
+        from = to
+        to = tmp
+    }
+
+    if (text) {
+        const res = await translate(text.trim(), { to })
+
+        return response([
+            {
+                Title: res.text,
+                Subtitle: `${from} -> ${to} parameters`,
+                IcoPath: ICON_PATH,
+                JsonRPCAction: {
+                    method: 'copy',
+                    parameters: [res.text],
+                    dontHideAfterAction: false,
+                },
+            },
+        ])
+    } else {
+        return response(DEFAULT_RESULT)
     }
 }
